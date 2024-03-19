88367e7a859cc57c505cec20ce487ae8139b84ba
lucasfogacadj <lucasfgc00@gmail.com>
2024-03-19 19:35:49 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int idade = 35;
+Console.WriteLine(idade);
+
+string nome = "Maria";
+Console.WriteLine(nome);
+
+double altura = 3.45;
+Console.WriteLine(altura);
+
+const int ano = 12;
+Console.WriteLine(ano);
+
+double? nota = 7.80;
+
+Console.WriteLine(nota);
+
+int x = 77, y =66;
+
+Console.WriteLine(x+y);
+
+//boll -> false
+//char -> '\o' ou (U+000)
+// int, double, float e decimal -> 0
+//string null
